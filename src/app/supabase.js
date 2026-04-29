d92ede73d926307b1950965880c738ec6b00ea67
Allan <allanebp04@gmail.com>
2026-04-29 05:29:10 -0600

    Variables para Vercel

--- src/app/supabase.js
+++ src/app/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://iukasubzadopzvrggtak.supabase.co";
-const supabaseKey = "sb_publishable_MmzDOEpP9FZD55cyXfvhnA_DRFN9SEz";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
